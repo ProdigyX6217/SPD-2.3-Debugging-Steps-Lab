1309ef3567b8ba7437df5b005460be80ed71e4d0
ProdigyX6217 <dominic.holder.15@cityas.org>
2021-01-24 16:21:14 -0700

    Fixed Exercise 1

--- exercise-1.py
+++ exercise-1.py
@@ -5,21 +5,27 @@ Exercise 1
 # PART 1: Gather Information
 #
 # TODO: Gather information about the source of the error and paste your findings here. E.g.:
-# - What is the expected vs. the actual output?
-# - What error message (if any) is there?
-# - What line number is causing the error?
-# - What can you deduce about the cause of the error?
+# - What is the expected vs. the actual output?   Expected Output is 4 while the Actual Output is a Stack Trace.
+# - What error message (if any) is there?   IndexError: list index out of range.
+# - What line number is causing the error?   File "exercise-1.py", line 23, in find_largest_diff.
+# - What can you deduce about the cause of the error?   the list index cannot be accessed properly.
+
+
+
 
 
 # PART 2: State Assumptions
 #
 # TODO: State your assumptions here or say them out loud to your partner ...
 # Make sure to be SPECIFIC about what each of your assumptions is!
+"""Line 23 attempts to access the next index in the list but reaches outside of the list.
+I resolved the bug by lowering the number of loops that occur in the for loop so that the loop would end before
+receiving IndexError: list index out of range."""
 
 def find_largest_diff(list_of_nums):
     """Find the largest difference between *consecutive* numbers in a list."""
     largest_diff = 0
-    for i in range(len(list_of_nums)):
+    for i in range(len(list_of_nums) - 1): # <-- fix by adding range(len(list_of_nums) - 1)
         diff = abs(list_of_nums[i] - list_of_nums[i+1])
         if diff > largest_diff:
             largest_diff = diff
