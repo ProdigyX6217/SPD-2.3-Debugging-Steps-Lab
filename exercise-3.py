7990976fc8d5b40addedd82f228105d90b4508a4
ProdigyX6217 <dominic.holder.15@cityas.org>
2021-01-24 17:35:22 -0700

    Fixed Exercise 4

--- exercise-3.py
+++ exercise-3.py
@@ -6,9 +6,9 @@ Exercise 3
 #
 # TODO: Gather information about the source of the error and paste your findings here. E.g.:
 # - What is the expected vs. the actual output?   Expected Output is a Sorted list while the Actual Output is a list index out of range error.
-# - What error message (if any) is there?   IndexError: list index out of range
-# - What line number is causing the error?   File "exercise-3.py", line 26, in insertion_sort
-# - What can you deduce about the cause of the error?
+# - What error message (if any) is there?   IndexError: list index out of range.
+# - What line number is causing the error?   File "exercise-3.py", line 26, in insertion_sort.
+# - What can you deduce about the cause of the error?   We are attempting to access an index outside of the list.
 
 
 # PART 2: State Assumptions
@@ -16,29 +16,22 @@ Exercise 3
 # TODO: State your assumptions here or say them out loud to your partner ...
 # Make sure to be SPECIFIC about what each of your assumptions is!
 # HINT: It may help to draw a picture to clarify what your assumptions are.
+"""The while loop subtracts 1 from j each loop, probable making j become a negative number."""
 
 def insertion_sort(arr):
     """Performs an Insertion Sort on the array arr."""
     for i in range(1, len(arr)):
-        key = arr[i] # 2
-        print("key", key)
+        key = arr[i] 
 
-        # sorted partition
-        j = i-1 # 0
-            # 2     # 5
-        while key < arr[j] and j >= 0: 
-            # 2         # 5
-            arr[j+1] = arr[j]
-            print("check", arr[j+1])
+        j = i-1
+        while j >= 0 and key < arr[j] : 
+            arr[j+1] = arr[j] 
             j -= 1
-            print("j", j)
         arr[j+1] = key
-        print("change", arr[j+1])
-        print(arr)
     return arr
 
 if __name__ == '__main__':
     print('### Problem 3 ###')
     answer = insertion_sort([5, 2, 3, 1, 6])
-    # print(answer)
+    print(answer)
 
--- exercise-4.py
+++ exercise-4.py
@@ -5,10 +5,10 @@ Exercise 4
 # PART 1: Gather Information
 #
 # TODO: Gather information about the source of the error and paste your findings here. E.g.:
-# - What is the expected vs. the actual output?
-# - What error message (if any) is there?
-# - What line number is causing the error?
-# - What can you deduce about the cause of the error?
+# - What is the expected vs. the actual output?   Expected Output is 4 while the Actual Output is a Recursion Error.
+# - What error message (if any) is there?   IndentationError: unindent does not match any outer indentation level / RecursionError: maximum recursion depth exceeded in comparison.
+# - What line number is causing the error?   File "exercise-4.py", line 22 -> line 37.
+# - What can you deduce about the cause of the error?   Recursion Error.
 
 
 # PART 2: State Assumptions
@@ -18,7 +18,7 @@ Exercise 4
 # HINT: It may help to draw a picture to clarify what your assumptions are.
 
 def binary_search(arr, element, low=0, high=None):
-      """Returns the index of the given element within the array by performing a binary search."""
+    """Returns the index of the given element within the array by performing a binary search."""
     if high == None:
         high = len(arr) - 1
 
@@ -31,12 +31,14 @@ def binary_search(arr, element, low=0, high=None):
         return mid
 
     elif arr[mid] > element:
-        return binary_search(arr, element, low, mid)
+        return binary_search(arr, element, low, mid-1)
 
     else: 
-        return binary_search(arr, element, mid, high)
+        return binary_search(arr, element, mid+1, high)
 
 
 if __name__ == '__main__':
-    answer = binary_search([1, 2, 4, 5, 7], 7)
+    answer = binary_search([1, 2, 4, 5, 7], 7) # 4
     print(answer)
+    # answer = binary_search([1, 2, 4, 5, 7], 1)  # 0
+    # print(answer)
